2655450b671062a4c7baa41bff39fcfc40f5f451
Vonaista <86628538+Vonaista@users.noreply.github.com>
2022-04-18 05:04:21 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5129838949:AAFs8aMV3581fj0-pv_B_bWTVYCwwQB_JxY"
+TOKEN = "5333049456:AAHShZqWmAzZzMHqP2x0_OArBIx7pTo70h4"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
